27ff0da0a786114cfdf68c9e2748bc9776fb7eaa
Manikandan-Rangarajan <116093831+Manikandan-Rangarajan@users.noreply.github.com>
2023-08-26 15:15:27 +0530

    Update server1.js

--- server1.js
+++ server1.js
@@ -6,7 +6,7 @@ import bodyParser from 'body-parser';
 
 const app = express()
 // const router = express.Router()
-const port = 8080 || process.env.PORT;
+const port = 8080;
 const __dirname = dirname(fileURLToPath(import.meta.url))
 // const input = Search();
 
@@ -30,6 +30,6 @@ app.get('/',(req,res)=>{
 //     // console.log(input.name)
 // });
 
-app.listen(port, ()=>{
+app.listen(port || process.env.PORT, ()=>{
     console.log("Server is running on "+port)
 })
